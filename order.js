a1f2d720d6f474f84a628cbb2cb1f29fdae1c308
jinShineTest <seungjin429+test@gmail.com>
2020-03-19 00:40:58 +0900

    add regex phonenumber

--- order.js
+++ order.js
@@ -27,7 +27,16 @@ function order() {
         alert("휴대폰번호를 입력해주세요")
         phone.focus()
         return
-    } 
+    } else if (!isPhoneNumber(phone.val())) {
+        alert('휴대폰번호 입력 형식이 틀립니다. \n예)010-0000-0000')
+        phone.focus()
+        return
+    }
 
     alert("주문 완료!")
+}
+
+function isPhoneNumber(number) {
+    let phoneRe = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+    return phoneRe.test(number);
 }
